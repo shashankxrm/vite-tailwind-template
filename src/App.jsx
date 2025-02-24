2db6c8383731e3db027960f1d1df7aeb5990adf9
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-02-24 19:33:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1 className="flex justify-center items-center h-screen text-4xl">
+        React App
+      </h1>
+    </div>
+  )
+}
+
+export default App
